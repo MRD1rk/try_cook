e15b3c557cb422bb0e677035853a7c72fba851f6
Ivan <i.hrymaliuk@msl.ua>
2019-09-02 17:23:57 +0300

    init admin theme

--- vendor/michele-angioni/phalcon-auth/src/PhalconAuth/Exceptions/RememberMeTokenExpired.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MicheleAngioni\PhalconAuth\Exceptions;
-
-use Phalcon\Exception;
-
-class RememberMeTokenExpired extends Exception
-{
-}
--- vendor/michele-angioni/phalcon-auth/src/PhalconAuth/Exceptions/WrongCredentialsException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MicheleAngioni\PhalconAuth\Exceptions;
-
-use Phalcon\Exception;
-
-class WrongCredentialsException extends Exception
-{
-}
